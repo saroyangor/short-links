9dcea9c59a6f91067050260d5ab05a18cfd68a9d
saroyangor <dr.saroyan98@gmail.com>
2022-06-30 17:08:25 +0400

    short links

---
+++ src/components/Shortens/Shortens.jsx
@@ -0,0 +1,49 @@
+import {useState} from "react"
+import {useSelector} from "react-redux"
+import {motion, AnimatePresence} from "framer-motion"
+
+import {selectLinks} from "../../store/slice/linkSlice"
+import {Button} from "components/Button"
+import classes from "./Shortens.module.scss"
+
+
+const Shortens = () => {
+    const [copiedLink, setCopiedLink] = useState(null)
+    const links = useSelector(selectLinks)
+
+    const copyToClipboard = (link) => {
+        navigator.clipboard.writeText(link).then(() => {
+            setCopiedLink(link)
+        })
+    }
+
+    if (!links?.length) return null
+
+    return (
+        <section className={classes.Shortens}>
+            <div className="container">
+                {links.map(item => (
+                    <AnimatePresence key={item.code}>
+                        <motion.div
+                            className={classes.item}
+                            data-active={copiedLink === item.full_short_link2}
+                            initial={{opacity: 0, height: 0}}
+                            animate={{opacity: 1, height: "auto"}}
+                        >
+                            <span>{item.original_link}</span>
+                            <span>{item.full_short_link2}</span>
+                            <Button
+                                variant="square"
+                                onClick={() => copyToClipboard(item.full_short_link2)}
+                            >
+                                {copiedLink === item.full_short_link2 ? "Copied!" : "Copy"}
+                            </Button>
+                        </motion.div>
+                    </AnimatePresence>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export {Shortens}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import {PersistGate} from "redux-persist/integration/react"
+
+import {App} from "./components/App"
+import {store} from "./store"
+import {persistor} from "./store"
+
+import "./index.css"
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={null}>
+                <App/>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
